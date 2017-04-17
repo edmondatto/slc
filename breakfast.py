b6f01680a3496e9bedd56f7d4b3103d1a46de29d
edmondatto <edmond091@gmail.com>
2017-04-17 17:18:51 +0300

    New branch (toe-dipping)

--- breakfast.py
+++ breakfast.py
@@ -1,2 +1,3 @@
 def make_breakfast(meal, *ingredients):
-    print("Yummy {} with {}".format(meal, "".join(ingredients)))
+    print("Creating something yumm")
+    print("Yummy {} with {}".format(meal, ", ".join(ingredients)))
